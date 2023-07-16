031bb9e4547f5186a83b597e8b1f05b0ef832df6
NikSne <niksne.mc@yandex.ru>
2023-07-16 15:40:53 +0300

    Code optimizations and simplifications

--- fabric/src/main/java/com/niksne/packetauth/server/auth/QueuePlayer.java
+++
@@ -1,9 +0,0 @@
-package com.niksne.packetauth.server.auth;
-
-import net.minecraft.server.network.ServerPlayerEntity;
-
-import java.util.concurrent.ScheduledExecutorService;
-
-public record QueuePlayer(ServerPlayerEntity player, ScheduledExecutorService service) {
-
-}

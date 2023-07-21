85a41d2ea3d0198f0b700849fd33a89f5bb1d728
NikSne <niksne.mc@yandex.ru>
2023-07-21 05:59:21 +0300

    v1.5.2

--- fabric/src/main/java/com/niksne/packetauth/server/Utils.java
+++ fabric/src/main/java/com/niksne/packetauth/server/Utils.java
@@ -5,7 +5,6 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtIo;
 
 import java.io.File;
-import java.sql.*;
 import java.util.Objects;
 
 public class Utils {
@@ -122,11 +121,13 @@ public class Utils {
                     } else {
                         x = parseFactor();
                     }
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else throw new RuntimeException("Unknown function: " + func);
+                    x = switch (func) {
+                        case "sqrt" -> Math.sqrt(x);
+                        case "sin" -> Math.sin(Math.toRadians(x));
+                        case "cos" -> Math.cos(Math.toRadians(x));
+                        case "tan" -> Math.tan(Math.toRadians(x));
+                        default -> throw new RuntimeException("Unknown function: " + func);
+                    };
                 } else {
                     throw new RuntimeException("Unexpected: " + (char)ch);
                 }

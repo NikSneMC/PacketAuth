780ab4856504b7020d79476bf96effeab074fe0b
NikSne <niksne.mc@yandex.ru>
2023-08-20 20:47:22 +0300

    v1.6
    Changelog:
    - Fabric config is now also yml
    - Now all configs are without comments
    - Added 1.18.x support
    - Now it will be notified if the client does not have the required version of the mod
    - Now for color codes you can use &
    - Added automatic token generator
    - Now the tokens are stored in a separate file
    - Added MySQL support
    
    Added new language:
    - Magyar (Hungarian) by [ichbinwhy](https://discprd.com/users/299224700213657601)

---
+++ plugin/src/main/java/com/niksne/packetauth/MigrateConfig.java
@@ -0,0 +1,25 @@
+package com.niksne.packetauth;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MigrateConfig {
+
+    public MigrateConfig(ConfigManager config, ConfigManager tokens) {
+        if (config.containsKey("config.version") && config.getString("config.version").equals("1.6")) return;
+        Map<String, String> old = new HashMap<>();
+        old.put("kick.message", config.getString("kick.message"));
+        config.removeString("kick.message");
+        old.put("kick.delay", config.getString("kick.delay"));
+        config.removeString("kick.delay");
+        tokens.overwrite(config.asMap());
+        config.clear();
+        config.putString("config.version", "1.6");
+        config.putString("kick.outdated", "&cYou need &aPacket Auth %version% or never &cto play on this server!");
+        config.putString("kick.message", old.getOrDefault("kick.message", "&cAuthorization error!"));
+        config.putString("kick.delay", old.getOrDefault("kick.delay", "%ping% + 200"));
+        config.putString("tokengen.enabled", "true");
+        config.putString("tokengen.length", "4096");
+        config.putString("tokengen.symbols", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+    }
+}
